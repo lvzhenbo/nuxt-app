4fa388436fd5e36807cc2a5fb0bf48b89cecba24
lvzhenbo <294221990@qq.com>
2022-09-16 17:45:31 +0800

    init

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <NConfigProvider
+    inline-theme-disabled
+    :locale="zhCN"
+    :date-locale="dateZhCN"
+  >
+  <div class="p-4">
+
+  </div>
+  </NConfigProvider>
+  <!-- <div>
+    <NuxtLayout>
+      <NuxtPage class="p-4" />
+    </NuxtLayout>
+  </div> -->
+</template>
+
+<script lang="ts" setup>
+import { NConfigProvider } from "naive-ui";
+import { zhCN, dateZhCN } from "naive-ui";
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  build: {
+    transpile:
+      process.env.NODE_ENV === "production"
+        ? [
+            "naive-ui",
+            "vueuc",
+            "@css-render/vue3-ssr",
+            "@juggle/resize-observer",
+            "date-fns", // 加入后可解决报错
+          ]
+        : ["@juggle/resize-observer"],
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === "development"
+          ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
+          : [],
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+});
